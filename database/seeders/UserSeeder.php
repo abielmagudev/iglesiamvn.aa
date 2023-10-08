e82f3b3c9a6c024470cc6a1852161f4bc2eedce3
Abiel Magu <abielmagu.dev@gmail.com>
2023-10-08 06:40:58 -0500

    Actualizacion menor en UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,7 +17,7 @@ class UserSeeder extends Seeder
     {
         User::create([
             'name' => 'iglesiamvn',
-            'email' => 'iglesiamvn@mail.com',
+            'email' => 'iglesiamvn@mail.dev',
             'email_verified_at' => now(),
             'password' => 'password', // $2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi
             'remember_token' => Str::random(10),
