c006664fb16adbc7a61f300e0328cccd048fd6a9
Abiel Magu <abielmagu.dev@gmail.com>
2023-10-08 06:39:55 -0500

    Actualizaciones menores, favicon y userseeder

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,7 @@
 
         <title>{{ config('app.name') }}</title>
 
-        <!-- Fonts -->
+        <link rel="icon" type="image/x-icon" href="{{ asset('images/mvn-favicon-b.png') }}">
         <link href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css" rel="stylesheet">
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" rel="stylesheet" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
